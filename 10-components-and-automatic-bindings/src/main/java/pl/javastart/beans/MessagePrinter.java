8bdddcf5b64ae17f1d01a50768d3965b05fc17e7
reszka303 <reszka303@op.pl>
2021-10-17 00:28:01 +0200

    13th commit: Spring Framework - Components and automatic bindings

---
+++ 10-components-and-automatic-bindings/src/main/java/pl/javastart/beans/MessagePrinter.java
@@ -0,0 +1,32 @@
+package pl.javastart.beans;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+
+@Component
+public class MessagePrinter {
+//    @Inject można alternatywnie stosować przy wiązaniu adnotację @Inject,
+//    korzystając z CDI, ale w praktyce się tego nie robi
+    @Autowired
+
+    private MessageProducer producer;
+
+    MessagePrinter() {}
+
+    public MessagePrinter(MessageProducer producer) {
+        this.producer = producer;
+    }
+
+    public MessageProducer getProducer() {
+        return producer;
+    }
+
+    public void setProducer(MessageProducer producer) {
+        this.producer = producer;
+    }
+
+    public void print() {
+        System.out.println(producer.getMessage());
+    }
+}
--- 10-components-and-automatic-bindings/src/main/java/pl/javastart/beans/SimpleMessageProducer.java
+++ 10-components-and-automatic-bindings/src/main/java/pl/javastart/beans/SimpleMessageProducer.java
@@ -1,8 +1,11 @@
 package pl.javastart.beans;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SimpleMessageProducer implements MessageProducer {
     @Override
     public String getMessage() {
-        return "Example message " + System.currentTimeMillis();
+        return "Simple Message: " + System.currentTimeMillis();
     }
 }
