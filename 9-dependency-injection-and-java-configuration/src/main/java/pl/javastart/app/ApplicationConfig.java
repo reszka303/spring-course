b47993c69de4621804bf512575ca8ed6a9c17ea4
reszka303 <reszka303@op.pl>
2021-10-16 03:03:30 +0200

    12th commit: Spring Framework - Dependency injection and java configuration

---
+++ 9-dependency-injection-and-java-configuration/src/main/java/pl/javastart/app/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package pl.javastart.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.javastart.beans.MessagePrinter;
+import pl.javastart.beans.MessageProducer;
+import pl.javastart.beans.SimpleMessageProducer;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean
+    public MessagePrinter getPrinter() {
+        MessageProducer producer = new SimpleMessageProducer();
+        return new MessagePrinter(producer);
+    }
+}
