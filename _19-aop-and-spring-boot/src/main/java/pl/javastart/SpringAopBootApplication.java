dfb9bad4f646aaf66463395d480f542377624f7d
reszka303 <reszka303@op.pl>
2021-10-23 00:53:42 +0200

    23rd commit: Spring AOP - JPA in Java SE

--- _19-aop-and-spring-boot/src/main/java/pl/javastart/SpringAopBootApplication.java
+++ _19-aop-and-spring-boot/src/main/java/pl/javastart/SpringAopBootApplication.java
@@ -1,29 +1,31 @@
-package pl.javastart;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import pl.javastart.model.Book;
-import pl.javastart.service.BookRepository;
-import pl.javastart.service.GenericRepository;
-
-@SpringBootApplication
-@EnableAspectJAutoProxy(proxyTargetClass = true)
-public class SpringAopBootApplication {
-    public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(SpringAopBootApplication.class);
-
-        GenericRepository<String, Book> repo = ctx.getBean(BookRepository.class);
-        repo.add(new Book("1111111111111", "Pierwsza", "Pierwszy autor"));
-        repo.add(new Book("2222222222222", "Druga", "Drugi autor"));
-        repo.add(new Book("3333333333333", "Trzecia", "Trzeci autor"));
-//        repo.add(null);
-//        Book book = repo.get("11111111");
-        Book book = repo.get("1111111111111");
-        System.out.println(book);
-
-        ctx.close();
-
-    }
-}
+//package pl.javastart;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.context.annotation.EnableAspectJAutoProxy;
+//import pl.javastart.model.Book;
+//import pl.javastart.service.BookRepository;
+//import pl.javastart.service.GenericRepository;
+//
+//import java.awt.print.Book;
+//
+//@SpringBootApplication
+//@EnableAspectJAutoProxy(proxyTargetClass = true)
+//public class SpringAopBootApplication {
+//    public static void main(String[] args) {
+//        ConfigurableApplicationContext ctx = SpringApplication.run(SpringAopBootApplication.class);
+//
+//        GenericRepository<String, Book> repo = ctx.getBean(BookRepository.class);
+//        repo.add(new Book("1111111111111", "Pierwsza", "Pierwszy autor"));
+//        repo.add(new Book("2222222222222", "Druga", "Drugi autor"));
+//        repo.add(new Book("3333333333333", "Trzecia", "Trzeci autor"));
+////        repo.add(null);
+////        Book book = repo.get("11111111");
+//        Book book = repo.get("1111111111111");
+//        System.out.println(book);
+//
+//        ctx.close();
+//
+//    }
+//}
---
+++ _20-jpa-in-java-se/src/main/java/pl/javastart/main/Main.java
@@ -0,0 +1,20 @@
+package pl.javastart.main;
+
+import pl.javastart.dao.BookDao;
+import pl.javastart.dao.BookDaoImpl;
+import pl.javastart.model.Book;
+
+public class Main {
+    public static void main(String[] args) {
+//        Book book = new Book("123456789", "Wilk z Wall Street", "Jordan Belfort");
+//        BookDao bookDao = new BookDaoImpl();
+//        bookDao.save(book);
+//        System.out.println("Book saved");
+//        bookDao.cleanUp();
+
+        BookDao bookDao = new BookDaoImpl();
+        Book book = bookDao.get(1L);
+        System.out.println(book);
+        bookDao.cleanUp();
+    }
+}
