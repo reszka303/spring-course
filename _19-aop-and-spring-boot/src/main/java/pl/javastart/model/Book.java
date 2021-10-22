2bb1763723cd19cf51ff4f2cffbb280633a6f843
reszka303 <reszka303@op.pl>
2021-10-22 02:46:02 +0200

    22nd commit: Spring AOP - Aop and Spring Boot

---
+++ _19-aop-and-spring-boot/src/main/java/pl/javastart/model/Book.java
@@ -0,0 +1,42 @@
+package pl.javastart.model;
+
+public class Book {
+    private String isbn;
+    private String title;
+    private String author;
+
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "isbn: " + isbn + ", " + "title: " + title + ", " + "author: " + author;
+    }
+}
