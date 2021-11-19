003ec3908c5d031cc170b43509fdd6cc9f04ed20
reszka303 <reszka303@op.pl>
2021-11-19 20:18:11 +0100

    48th commit: Spring MVC - Spring MVC configuration in spring boot

---
+++ _43-spring-mvc-configuration-in-spring-boot/src/main/java/pl/SpringMvcBootWarApplication.java
@@ -0,0 +1,12 @@
+package pl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBootWarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcBootWarApplication.class, args);
+    }
+}
---
+++ _43-spring-mvc-configuration-in-spring-boot/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,8 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<head>
+    <title>Homepage</title>
+</head>
+<body>
+<h1>Spring Boot webapp</h1>
+</body>
+</html>
