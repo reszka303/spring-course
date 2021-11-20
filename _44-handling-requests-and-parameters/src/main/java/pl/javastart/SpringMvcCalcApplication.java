fba13c90c2ad4ef7ba7198a211f1cf8b45099a73
reszka303 <reszka303@op.pl>
2021-11-20 21:29:08 +0100

    49th commit: Spring MVC - Handling requests and parameters

---
+++ _44-handling-requests-and-parameters/src/main/java/pl/javastart/SpringMvcCalcApplication.java
@@ -0,0 +1,11 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCalcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcCalcApplication.class, args);
+    }
+}
