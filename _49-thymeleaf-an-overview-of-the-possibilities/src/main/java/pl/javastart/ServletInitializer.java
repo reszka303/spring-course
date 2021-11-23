465c993ea127e2152b3ea67c46870a0abc840c97
reszka303 <reszka303@op.pl>
2021-11-23 18:15:41 +0100

    55th commit: Spring MVC - Thymeleaf an overview of the possibilities

---
+++ _49-thymeleaf-an-overview-of-the-possibilities/src/main/java/pl/javastart/ServletInitializer.java
@@ -0,0 +1,16 @@
+package pl.javastart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringMvcThymeleafMoreApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcThymeleafMoreApplication.class);
+    }
+}
---
+++ _49-thymeleaf-an-overview-of-the-possibilities/src/main/java/pl/javastart/controller/AppController.java
@@ -0,0 +1,48 @@
+package pl.javastart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.javastart.model.Book;
+
+import java.util.Arrays;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String home(Model model) {
+        model.addAttribute("book", new Book());
+        return "home";
+    }
+
+    @GetMapping("/if")
+    public String ifExample(Model model) {
+        model.addAttribute("text", "Sample text");
+        model.addAttribute("a", 123);
+        model.addAttribute("b", 456);
+        return "if";
+    }
+
+    @GetMapping("/loop")
+    public String loopExample(Model model) {
+        model.addAttribute("books", Arrays.asList(
+                new Book("Książka 1", "Autor 1", 59.99),
+                new Book("Książka 2", "Autor 2", 69.99),
+                new Book("Książka 3", "Autor 3", 79.99),
+                new Book("Książka 4", "Autor 4", 89.99),
+                new Book("Książka 5", "Autor 5", 99.99),
+                new Book("Książka 6", "Autor 6", 19.99)
+        ));
+        return "loop";
+    }
+
+    @PostMapping("/bookadd")
+    public String bookAdd(@ModelAttribute Book book, Model model) {
+        model.addAttribute("message", "Book " + book + " added!");
+        return "home";
+    }
+}
