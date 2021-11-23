b357566f90c87f5f4353acf1e2fd0613961f2e81
reszka303 <reszka303@op.pl>
2021-11-23 23:07:36 +0100

    56th commit: Spring MVC - Spring MVC + Spring Data + Thymeleaf

---
+++ _50-spring-mvc-spring-data-thymeleaf/src/main/java/pl/javastart/controller/TimeDataController.java
@@ -0,0 +1,35 @@
+package pl.javastart.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import pl.javastart.data.TimeDataRepository;
+import pl.javastart.model.TimeData;
+
+@Controller
+public class TimeDataController {
+
+    private TimeDataRepository timeDataRepository;
+
+    @Autowired
+    public TimeDataController(TimeDataRepository timeDataRepository) {
+        this.timeDataRepository = timeDataRepository;
+    }
+
+    @PostMapping("/save")
+    public String saveTime(@ModelAttribute TimeData timeData) {
+        timeDataRepository.save(timeData);
+        return "redirect:/";
+    }
+
+    @GetMapping("/show")
+    public String showAll(Model model) {
+        List<TimeData> allTimes = timeDataRepository.findAll();
+        model.addAttribute("allTimes", allTimes);
+        return "showall";
+    }
+}
